86d76ffba82e460644b2e14c2cd390ca57aa8830
arcsocial <arcsoocial@gmail.com>
2025-05-17 14:46:30 +0530

    Update library-api-client.js

--- js/library-api-client.js
+++ js/library-api-client.js
@@ -166,7 +166,7 @@ async function getCSVData(filename) {
     const data = await response.text();
     
     const rows = data.split('\n');
-    console.log('Rows:', rows):
+    console.log('Rows:', rows);
     const headers = rows[0].split('|');
     const items = rows.slice(1).map(row => {
       const values = row.split('|');
